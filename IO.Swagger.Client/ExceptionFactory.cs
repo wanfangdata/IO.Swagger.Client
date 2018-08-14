0c411211feccf540a7f139a3a4218257a74115f8
hueidou <hueidou163@gmail.com>
2018-08-14 09:39:38 +0800

    first commit

---
+++ IO.Swagger.Client/ExceptionFactory.cs
@@ -0,0 +1,13 @@
+using RestSharp;
+using System;
+
+namespace IO.Swagger.Client
+{
+    /// <summary>
+    /// A delegate to ExceptionFactory method
+    /// </summary>
+    /// <param name="methodName">Method name</param>
+    /// <param name="response">Response</param>
+    /// <returns>Exceptions</returns>
+    public delegate Exception ExceptionFactory(string methodName, IRestResponse response);
+}
